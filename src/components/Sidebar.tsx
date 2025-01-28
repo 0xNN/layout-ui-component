23ff0b57f59de5bf30500fa8fb8e36c5ba9402e5
Muhammad Sendi Noviansyah <muhammad.noviansyah@puninar.com>
2025-01-28 14:44:31 +0700

    Change strokeWidth Searching Icon

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -99,7 +99,7 @@ class Sidebar extends Component<SidebarProps, SidebarState> {
               {showSearch && (
                 <div className={`mb-4 px-4 pt-4 delay-100 ${isCollapsed && !isHovered ? 'hidden' : ''}`}>
                   <div className="flex items-center border border-blue-400 rounded-lg bg-blue-100">
-                    <svg style={{ marginLeft: 10 }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <svg style={{ marginLeft: 10 }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="size-6">
                       <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                     </svg>
                     <input
